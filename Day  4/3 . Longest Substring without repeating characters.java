b61323d27edbfa736fde78caa8dc5616fb814593
Dinesh <106336817+DineshPrabakaran@users.noreply.github.com>
2022-12-19 18:50:53 +0530

    Longest Substring without repeating characters

--- Day  4/3 . Longest Substring without repeating characters.java
+++ Day  4/3 . Longest Substring without repeating characters.java
@@ -23,3 +23,20 @@ class Solution {
         return true;
     }
 }
+//Sliding Window Solution optimized
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int max = 0;
+        int n = s.length();
+        for(int i=0,j=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                j = Math.max(j,map.get(ch));
+            }
+            max = Math.max(max,i-j+1);
+            map.put(ch,i+1);
+        }
+        return max;
+    }
+}
